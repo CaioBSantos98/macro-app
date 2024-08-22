e1be9ae579b0f57604c870be2de1d8ce4a8a1333
Caio Belchior <caio.belchior@hotmail.com>
2024-08-22 16:03:39 -0300

    add authentication form to login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,45 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <Box component="section" sx={{ p: 2, display: "flex", justifyContent: "center" }}>
+            <Box component="form" sx={{
+                maxWidth: "350px",
+                width: "100%",
+                height: "100%",
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "20px",
+                border: "2px solid grey",
+                padding: "20px",
+                marginTop: "50px"
+            }}>
+                <Typography variant="h4" component="h2">Faça seu login</Typography>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Email"
+                    type="email"
+                    defaultValue=""
+                    sx={{width: "100%"}}
+                />
+                <TextField
+                    required
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    sx={{width: "100%"}}
+                />
+                <Button variant="contained" sx={{
+                    width: "100%",
+                    height: "56px",
+                    fontSize: "16px"
+                }}>Entrar</Button>
+            </Box>
+        </Box>
     )
 }
 
