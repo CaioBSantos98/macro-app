3d8ff40030f3937a2650eecf70fc25582d909d62
Caio Belchior <caio.belchior@hotmail.com>
2024-08-23 16:12:53 -0300

    fixed login form

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -26,44 +26,51 @@ const LoginForm = () => {
     }
 
     return (
-        <Box component="form" sx={{
-            maxWidth: "350px",
-            width: "100%",
-            height: "100%",
-            borderRadius: 4,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "20px",
-            border: "2px solid grey",
-            padding: "20px",
-            marginTop: "50px"
-        }}
-            onSubmit={(event) => submitHandler(event)}>
-            <Typography variant="h4" component="h2">Faça seu login</Typography>
-            <TextField
-                required
-                autoComplete="username"
-                label="Email"
-                type="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                sx={{ width: "100%" }}
-            />
-            <TextField
-                required
-                autoComplete="current-password"
-                label="Senha"
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                sx={{ width: "100%" }}
-            />
-            <Button variant="contained" type="submit" sx={{
+        <Box
+            sx={{
+                maxWidth: "350px",
                 width: "100%",
-                height: "56px",
-                fontSize: "16px"
-            }}>Entrar</Button>
+                height: "100%",
+                marginTop: "50px",
+                borderRadius: 4,
+                border: "2px solid grey",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+            <Box component="form" onSubmit={(event) => submitHandler(event)} sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "20px",
+                padding: "20px",
+                width: "100%"
+            }}>
+                <Typography variant="h4" component="h2">Faça seu login</Typography>
+                <TextField
+                    required
+                    autoComplete="username"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    sx={{ width: "100%" }}
+                />
+                <TextField
+                    required
+                    autoComplete="current-password"
+                    label="Senha"
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    sx={{ width: "100%" }}
+                />
+                <Button variant="contained" type="submit" sx={{
+                    width: "100%",
+                    height: "56px",
+                    fontSize: "16px"
+                }}>Entrar</Button>
+            </Box>
             <Typography variant="overline" sx={{ display: "flex", gap: "5px" }}>
                 Não possui conta?
                 <NewUserModal />
