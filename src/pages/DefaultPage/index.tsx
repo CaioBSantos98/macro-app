3deb8afb46ceee9b5abfa14628e2356874c8da5a
Caio Belchior <caio.belchior@hotmail.com>
2024-08-23 09:39:32 -0300

    checking if the user is authenticated when starting the application

--- src/pages/DefaultPage/index.tsx
+++ src/pages/DefaultPage/index.tsx
@@ -2,8 +2,25 @@ import { AppBar, Box, Container, Toolbar } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import { useEffect } from "react";
+import { getUser } from "../../utils/login";
+import { useRecoilState } from "recoil";
+import { authState } from "../../state/atom";
 
 const DefaultPage = () => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [auth, setAuthState] = useRecoilState(authState);
+
+    useEffect(() => {
+        getUser((userDetails) => {
+            setAuthState({
+                isAuthenticated: true,
+                ...userDetails
+            })
+        });
+    }, [setAuthState])
+
     return (
         <Box component="main" sx={{minHeight: "100vh", display: "flex", flexDirection: "column"}}>
             <AppBar position="static">
--- src/utils/login.ts
+++ src/utils/login.ts
@@ -13,3 +13,13 @@ export const login = async (email: string, password: string, onSucess: (userDeta
         console.error('Error during login', error);
     }
 }
+
+export const getUser = async (onSucess: (userDetails: IUserDetails) => void) => {
+    try {
+        const response = await axios.get(`${url}/api/user`);
+        const data: IUserDetails = response.data;
+        onSucess(data)
+    } catch (error) {
+        console.error('Error during get user', error);
+    }
+}
